bff36bd5f11ceaa6aeaa29999971d2ac10a42eb1
Facundo Serna <30941820+FacundoSerna96@users.noreply.github.com>
2023-12-06 17:11:35 -0300

    Se agrega bucle para intentar de nuevo la llamada
    también se aumenta el limite de nodos hijos

--- app.js
+++ app.js
@@ -35,23 +35,24 @@ app.post("/migracionCO", async (req, res) => {
   };
 
   await fetch(
-    `${process.env.API}/-default-/public/alfresco/versions/1/nodes/${uuidOrigen}/children?skipCount=0&maxItems=100&include=path`,
+    `${process.env.API}/-default-/public/alfresco/versions/1/nodes/${uuidOrigen}/children?maxItems=1000000`,
     requestOptions
   )
     .then((response) => response.json())
     .then((result) => {
+      console.log("resultado de obtener todos los nodos: ", result)
       writeToLog(`Obtengo los hijos del id: ${uuidOrigen}`);
       console.log(`Obtengo los hijos del id: ${uuidOrigen}`);
       result["list"]["entries"].forEach((f) => {
         cantLote++;
 
         if (cantLote > loteMax) {
-          writeToLog(
+          /*  writeToLog(
             `Se termina la ejecucion por superar el limite del lote de ${loteMax}`
           );
           console.log(
             `Se termina la ejecucion por superar el limite del lote de ${loteMax}`
-          );
+          ); */
           return;
         }
 
@@ -65,7 +66,7 @@ app.post("/migracionCO", async (req, res) => {
 
         // Desestructurar la fecha en sus componentes
         const year = date.getFullYear();
-        const month = date.getMonth(); // Ten en cuenta que en JavaScript los meses comienzan desde 0 (enero es 0, febrero es 1, etc.)
+        const month = date.getMonth() + 1; // Ten en cuenta que en JavaScript los meses comienzan desde 0 (enero es 0, febrero es 1, etc.)
         const day = date.getDate();
 
         //verifico si existe algun id en el localstorage
@@ -154,17 +155,25 @@ const encontrarCarpeta = async (year, month, day, idOrigen, uuidDestino) => {
   )
     .then((response) => response.json())
     .then((result) => {
-      console.log(
-        `Se encuentra la carpeta con esta fecha: ${day}/${month}/${year}`
-      );
-      writeToLog(
-        `Se encuentra la carpeta con esta fecha: ${day}/${month}/${year}`
-      );
-      result["list"]["entries"].forEach((d) => {
-        if (d["entry"]["name"] == day) {
-          moverCarpeta(idOrigen, d["entry"]["id"]);
-        }
-      });
+      console.log(`Se encuentra la carpeta con esta fecha: ${year}/${month}`);
+      writeToLog(`Se encuentra la carpeta con esta fecha: ${year}/${month}`);
+
+      //en el caso de que la carpeta este vacia
+      if (result["list"]) {
+        result["list"]["entries"].forEach((d) => {
+          if (d["entry"]["name"] == day) {
+            moverCarpeta(idOrigen, d["entry"]["id"]);
+          }
+        });
+      } else {
+        console.log("resultados al fallar en obtencion fecha: ", result);
+        console.log(
+          `Error al obtener carpeta con fecha: ${day}/${month}/${year}`
+        );
+        writeToLog(
+          `Error al obtener carpeta con fecha: ${day}/${month}/${year}`
+        );
+      }
     })
     .catch((error) => {
       console.log("error en encontrarCarpeta: ", error);
@@ -202,7 +211,11 @@ const moverCarpeta = async (uuidOrigen, uuidDestino) => {
       writeToLog(
         `Se mueve la carpeta con id : ${uuidOrigen} a la carpeta con id: ${uuidDestino}`
       );
-      extraerHijosGR(result["entry"]["id"], result["entry"]["name"]);
+
+      // en el caso de que la carpeta este vacia
+      if (result["entry"]) {
+        extraerHijosGR(result["entry"]["id"], result["entry"]["name"]);
+      }
     })
     .catch((error) => {
       console.log("error en moverCarpeta: ", error);
@@ -222,21 +235,25 @@ const extraerHijosGR = async (id, name) => {
     redirect: "follow",
   };
 
-  await fetch(
-    `${process.env.API}/-default-/public/alfresco/versions/1/nodes/${id}/children?skipCount=0&maxItems=100`,
-    requestOptions
-  )
-    .then((response) => response.json())
-    .then((hijosGR) => {
-      console.log(`Se logran extraer los hijos con id: ${id}`);
-      writeToLog(`Se logran extraer los hijos con id: ${id}`);
-      buscarEnCO(name, hijosGR["list"]["entries"]);
-    })
-    .catch((error) => {
-      console.log("error en extraerHijos: ", error);
-      writeToLog(`Error al extraer hijos con id: ${id}`);
-      localStorage.setItem("idBackupError", idBackupError);
-    });
+  let extraerHijos = false
+  do {
+    await fetch(
+      `${process.env.API}/-default-/public/alfresco/versions/1/nodes/${id}/children?skipCount=0&maxItems=100`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((hijosGR) => {
+        console.log(`Se logran extraer los hijos con id: ${id}`);
+        writeToLog(`Se logran extraer los hijos con id: ${id}`);
+        buscarEnCO(name, hijosGR["list"]["entries"]);
+        extraerHijos = true;
+      })
+      .catch((error) => {
+        console.log("error en extraerHijos: ", error, " se intenta de nuevo...");
+        writeToLog(`Error al extraer hijos con id: ${id} , se intenta de nuevo...`);
+        localStorage.setItem("idBackupError", idBackupError);
+      });
+  } while (!extraerHijos);
 };
 
 const buscarEnCO = async (name, hijosGR) => {
@@ -279,11 +296,10 @@ const buscarEnCO = async (name, hijosGR) => {
       }
     })
     .catch((error) => {
-      console.log(`Error al buscan carpetas en con con el nombre: ${name}`);
-      console.log("error: ", error);
-      writeToLog(`Error al buscan carpetas en con con el nombre: ${name}`);
+      console.log(`No se encuentran carpetas con con el nombre: ${name}`);
+      writeToLog(`No se encuentran carpetas con con el nombre: ${name}`);
 
-      localStorage.setItem("idBackupError", idBackupError);
+      //localStorage.setItem("idBackupError", idBackupError);
     });
 };
 
@@ -324,6 +340,12 @@ const compararDuplicados = async (hijosGR, hijosCO) => {
   var arrayNombre = [];
   var arrayId = [];
 
+  //si no hay elementos encontrados de CO
+  //no hace falta la comparacion
+  if (hijosCO.length == 0) {
+    return;
+  }
+
   hijosCO.forEach((h) => {
     arrayNombreCO.push(h["entry"]["name"]);
   });
@@ -349,8 +371,12 @@ const compararDuplicados = async (hijosGR, hijosCO) => {
       writeToLog(
         `No se encuentra archivo duplicado con nombre: ${h["entry"]["name"]}`
       );
-      destino = hijosGR[0]["entry"]["parentId"];
-      documentosMover.push(h["entry"]["id"]);
+
+      //en el caso de que la carpeta este vacia
+      if (hijosGR[0]) {
+        destino = hijosGR[0]["entry"]["parentId"];
+        documentosMover.push(h["entry"]["id"]);
+      }
     }
   });
 
@@ -449,12 +475,12 @@ const borrarCO = async (idAborrar) => {
     .then((response) => response.json())
     .then((result) => {
       console.log(`La carpeta de CO con id: ${idAborrar} , se borro con exto`);
-      writeToLog(
-        `La carpeta de CO con id: ${idAborrar} , se borro con exito`
-      );
+      writeToLog(`La carpeta de CO con id: ${idAborrar} , se borro con exito`);
     })
     .catch((error) => {
-      console.log(`ERROR: La carpeta de CO con id: ${idAborrar} , no se pudo borrar, error: ${error}`);
+      console.log(
+        `ERROR: La carpeta de CO con id: ${idAborrar} , no se pudo borrar, error: ${error}`
+      );
       writeToLog(
         `ERROR: La carpeta de CO con id: ${idAborrar} , no se pudo borrar, error: ${error}`
       );
